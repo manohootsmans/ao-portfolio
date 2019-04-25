c974cefc37b221efdb093349f87ec2b8686383d5
Mano <manohootsmans@gmail.com>
2019-04-25 15:25:06 +0200

    Started

--- index.php
+++ index.php
@@ -10,14 +10,77 @@
 
     <link rel="stylesheet" href="css/master.css">
 
-    <title>Mano Hootsmans | Web developer</title>
+    <title>Mano Hootsmans | Webdeveloper</title>
   </head>
   <body>
-    <div class="container-fluid landing justify-content-center">
-      <div class="my-container justify-content-center">
+    <div class="landing-page">
+      <div class="container">
+
+        <nav class="navbar navbar-light">
+          <a class="navbar-brand" href="#">
+            <img src="img/logo.svg" alt="">
+          </a>
+          <ul class="nav smooth-scroll justify-content-end">
+            <a class="nav-link js-scroll-trigger" href="#">Home</a>
+            <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
+            <a class="nav-link" href="#">About me</a>
+            <a class="nav-link" href="#">Contact</a>
+          </ul>
+        </nav>
+
+        <h1 class="title">Mano Hootsmans</h1>
+        <h2 class="sub-title">Webdeveloper.</h2>
+
+        <img class="illustration" src="img/illustration.svg" alt="">
+
+        <div class="">
+          <a href="#projects"><img class="arrow-down" src="img/arrow-down.svg" alt=""></a>
+        </div>
 
       </div>
     </div>
+
+    <div id="projects"class="projects-page">
+      <div class="container">
+
+        <h2 class="page-title col-12">Projects</h2>
+
+        <div class="row">
+          <div class="project-item col-5">
+            <h3 class="white">WijWhy.nl</h3>
+
+            <div class="project-img-container">
+              <img class="project-img" src="img/wijwhy.nl.png" alt="">
+              <div class="overlay overlayBottom" href="google.nl">
+                <div class="overlay-text"> <a href="https://www.google.com">Click for more info</a> </div>
+
+              </div>
+            </div>
+
+          </div>
+
+          <div class="project-item offset-2 col-5">
+            <h3 class="white">Website.nl</h3>
+            <img class="project-img" src="https://via.placeholder.com/1920x1080" alt="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="project-item col-5">
+            <h3 class="white">Website.nl</h3>
+            <img class="project-img" src="https://via.placeholder.com/1920x1080" alt="">
+          </div>
+
+          <div class="project-item offset-2 col-5">
+            <h3 class="white">Website.nl</h3>
+            <img class="project-img" src="https://via.placeholder.com/1920x1080" alt="">
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
