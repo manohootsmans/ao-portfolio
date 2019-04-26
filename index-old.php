f3da40848c174cd3fd5eae05b6be769c6b71e0e0
Mano <manohootsmans@gmail.com>
2019-04-26 13:52:05 +0200

    Backup

---
+++ index-old.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/master-old.css">
+
+    <title>Mano Hootsmans | Webdeveloper</title>
+  </head>
+  <body>
+    <div class="landing-page">
+      <div class="container">
+
+        <nav class="navbar nav navbar-light">
+          <a class="navbar-brand" href="#"><img src="img/logo.svg" alt=""></a>
+
+            <ul class="nav justify-content-end">
+              <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
+              <a class="nav-link" href="#">About me</a>
+              <a class="nav-link" href="#">Contact</a>
+            </ul>
+          </nav>
+
+        <div class="row align-items-center">
+          <div class="col-12 d-none d-md-block  col-md-6 title-container">
+            <h1 class="title">Mano Hootsmans</h1>
+            <h2 class="sub-title">Webdeveloper.</h2>
+          </div>
+
+          <div class="offset-md-1 offset-2 offset-lg-0 col-8 col-md-5">
+            <img class="illustration" src="img/illustration.svg" alt="">
+          </div>
+
+          <div class="d-md-none col-12  col-md-6 title-container">
+            <h1 class="title">Mano Hootsmans</h1>
+            <h2 class="sub-title">Webdeveloper.</h2>
+          </div>
+
+        </div>
+
+
+        <div class="">
+          <a href="#projects"><img class="arrow-down" src="img/arrow-down.svg" alt=""></a>
+        </div>
+
+      </div>
+    </div>
+
+    <div id="projects"class="projects-page">
+      <div class="container">
+
+        <h2 class="page-title col-12">Projects</h2>
+
+        <div class="row">
+          <div class="project-item col-12 col-md-5">
+            <h3 class="white">WijWhy.nl</h3>
+            <div class="project-img-container">
+              <img class="project-img" src="img/wijwhy.nl.png" alt="">
+              <div class="overlay overlayBottom">
+                <div class="overlay-text"> <a href="#">More info</a> </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="project-item col-12 col-md-5 offset-md-2">
+            <h3 class="white">Website.com</h3>
+            <div class="project-img-container">
+              <img class="project-img" src="https://via.placeholder.com/1920x1080" alt="">
+              <div class="overlay overlayBottom">
+                <div class="overlay-text"> <a href="#">More info</a> </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="project-item col-12 col-md-5">
+            <h3 class="white">Website.com</h3>
+            <div class="project-img-container">
+              <img class="project-img" src="https://via.placeholder.com/1920x1080" alt="">
+              <div class="overlay overlayBottom">
+                <div class="overlay-text"> <a href="#">More info</a> </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="project-item col-12 col-md-5 offset-md-2">
+            <h3 class="white">Website.com</h3>
+            <div class="project-img-container">
+              <img class="project-img" src="https://via.placeholder.com/1920x1080" alt="">
+              <div class="overlay overlayBottom">
+                <div class="overlay-text"> <a href="#">More info</a> </div>
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
